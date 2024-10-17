f98d018cdb6b2ba95f65c9c23e4b596b7395d1e6
Seeingu <seeingasu@gmail.com>
2024-10-17 16:53:52 +0800

    add a simple repl

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ For learning purpose.
 
 This is a zig implementation of [Rui](https://github.com/rui314)'s [minilisp](https://github.com/rui314/minilisp). 
 
-## Build and test
+## Build and Test
 
-clone dependencies `git submodule update --init`
+To build and run the project, use:
+```sh
+zig build run
+```
 
-run `zig build test`
+To run tests, execute:
+```sh
+zig build test
+```
--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,28 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
 const std = @import("std");
 const evaluator = @import("evaluator.zig");
 
 pub fn main() !void {
-    const r = try evaluator.run("(defun f (x) (if (= x 0) 0 (+ (f (+ x -1)) x))) (f 2)");
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout);
+    const out = bw.writer();
 
-    try stdout.print("Result:\n{s}\n", .{r});
+    var line: [256]u8 = undefined;
 
-    try bw.flush(); // Don't forget to flush!
+    while (true) {
+        try out.print("> ", .{});
+        try bw.flush();
+
+        const read_bytes = try stdin.readUntilDelimiterOrEof(&line, '\n');
+        if (read_bytes) |bytes| {
+            if (std.mem.eql(u8, bytes, "exit")) break;
+            const result = try evaluator.run(bytes);
+            try out.print("{s}\n", .{result});
+            try bw.flush();
+        } else {
+            break;
+        }
+    }
 }
 
 test "all" {
